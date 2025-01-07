d5778d8399689bf95c71de338be69f96fb4c1ff7
Ryan McBrayer <drmcbrayer89@gmail.com>
2025-01-07 13:43:27 -0500

    added cmake & makefile

---
+++ build/CMakeFiles/MyEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyEngine.dir/link.d"
+  "CMakeFiles/MyEngine.dir/main.c.o"
+  "CMakeFiles/MyEngine.dir/main.c.o.d"
+  "MyEngine"
+  "MyEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
